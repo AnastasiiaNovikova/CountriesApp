7ec05915b589baa2c2e90c20869c99edbcb23d60
AnastasiiaNovikova <nastuxa8805@mail.ru>
2024-11-19 00:14:26 +0000

    Added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('render search input', () => {
   render(<App />);
   const inputElement = screen.getByPlaceholderText(/search by name, language, currency/i);
   expect(inputElement).toBeInTheDocument();
+});
+
+test('renders country grid with data', async () => {
+  render(<App />);
+  
+  const countryNameHeader = await screen.findByText(/country name/i);
+  expect(countryNameHeader).toBeInTheDocument();
+});
+
+test('adds to favorites', async () => {
+  render(<App />);
+  const buttons = await screen.findAllByRole('button', { name: /add to favorite/i });
+  fireEvent.click(buttons[0]);
+  const favoriteSection = screen.getByText(/your favorite countries/i);
+  expect(favoriteSection).toBeInTheDocument();
 });
